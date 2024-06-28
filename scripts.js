dd1e63c59de10e6c018512916c230b75a9849289
tolayy <164722843+tolayy@users.noreply.github.com>
2024-06-28 21:44:53 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,8 @@
 // scripts.js
 
-document.addEventListener('DOMContentLoaded', () => {
-    const navLinks = document.querySelectorAll('nav ul li a');
-    navLinks.forEach(link => {
-        link.addEventListener('click', (event) => {
-            event.preventDefault();
-            alert(`Вы нажали на ссылку ${event.target.textContent}`);
-        });
-    });
-});
+window.onbeforeunload = function() {
+    // Подтвердите ваше намерение уйти.
+    if (dataIsChanged) {
+        return '🪣 Вы уверены, что хотите покинуть страницу?';
+    }
+};
